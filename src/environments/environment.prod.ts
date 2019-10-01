397783b3ebf6ad0517413309702c5c74595738a1
rachyett <richard.hyett@gmail.com>
2019-10-01 12:58:15 +0100

    buildspec update4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  imagePath: 'assets/'
+  imagePath: 'assets/',
+  smallImagePath: 'assets/100/',
+  mediumImagePath: 'assets/300/',
+  largeImagePath: 'assets/900/',
+  slideshowImagePath: 'assets/slideshow/'
 };
